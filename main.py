b1ef84b8baac0e8528cd910408702084419d5021
LLZ14 <90233785+LLZ14@users.noreply.github.com>
2022-10-07 11:40:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -58,4 +58,3 @@ wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(),"color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
-print(get_proe())
