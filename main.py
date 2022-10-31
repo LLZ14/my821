3dd88cbc95d9dfca65017c859965eade8b15104b
LLZ14 <90233785+LLZ14@users.noreply.github.com>
2022-11-01 02:02:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def get_birthday1():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/")
+  words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
