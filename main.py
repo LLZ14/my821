9fab5684900dbc6da2d56bc25dbbf8dc5873a474
LLZ14 <90233785+LLZ14@users.noreply.github.com>
2022-11-07 12:40:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -53,6 +53,10 @@ def get_words():
     return get_words()
   return words.json()['data']['text']
 
+def get_words1():
+  words = "日光给你镀上成熟，月华增添你的妩媚，生日来临之际，愿朋友的祝福汇成你快乐的源泉愿祝福萦绕着你，在你永远与春天接壤的梦幻里。祝你：心想事成幸福快乐！生日快乐！"
+  return words
+
 # def get_proe():
 #   proe = requests.get("http://yijuzhan.com/api/word.php?m=json")
 #   if proe.status_code != 200:
@@ -71,18 +75,46 @@ def get_words():
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
   #return "#3399FF"
+  
+def get_random_color_to_birthday():
+  return "red"
 
 
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea,temperature = get_weather()
-# proe = get_proe()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(),"color":get_random_color()}}
-data1 = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count1()},"birthday_left":{"value":get_birthday1()},"words":{"value":get_words(),"color":get_random_color()}}
 
-res = wm.send_template(user_id, template_id, data)
-res1 = wm.send_template(user_id1,template_id,data1)
+next1 = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+print(next1)
+print(datetime.now())
+bday = (next1 - today).days
+# if next1 == datetime.now():
+#     # next1 = next1.replace(year=next1.year + 1)
+#     bday = (next1 - today).days
+
+
+
+if bday == 0 :
+  data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words1(),"color":get_random_color_to_birthday()}}
+# res = wm.send_template(user_id, template_id, data)
+  res1 = wm.send_template(user_id,template_id,data)
+  print(res1)
+else:
+  data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(),"color":get_random_color()}}
+# res = wm.send_template(user_id, template_id, data)
+  #res1 = wm.send_template(user_id,template_id,data)
+  #data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(),"color":get_random_color()}}
+  data1 = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count1()},"birthday_left":{"value":get_birthday1()},"words":{"value":get_words(),"color":get_random_color()}}
+  res = wm.send_template(user_id, template_id, data)
+  res1 = wm.send_template(user_id1,template_id,data1)
+
+# proe = get_proe()
+#data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(),"color":get_random_color()}}
+#data1 = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count1()},"birthday_left":{"value":get_birthday1()},"words":{"value":get_words(),"color":get_random_color()}}
+
+#res = wm.send_template(user_id, template_id, data)
+#res1 = wm.send_template(user_id1,template_id,data1)
 print(res)
 print(res1)
 
